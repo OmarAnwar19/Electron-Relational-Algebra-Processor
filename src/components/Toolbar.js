d4e3cba61200d844678f926a51bb3aa26424b9ed
OmarAnwar19 <omaranwar04@outlook.com>
2024-01-12 00:39:10 -0500

    [dev] union button style

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -4,45 +4,40 @@ import { icons } from "../lib/iconsOps"; // import the icons object
 
 // define the type of the Toolbar component
 const Toolbar = ({ onClick }) => {
+  const buttonStyle = {
+    padding: "8px",
+    fontSize: "16px",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
   return (
     <ButtonGroup style={{ width: "100%" }}>
-      <Button
-        style={{ padding: "8px" }}
-        onClick={() => onClick(`${icons.sigma}`)}
-      >
+      <Button style={buttonStyle} onClick={() => onClick(`${icons.sigma}`)}>
         {icons.sigma}
       </Button>
-      <Button style={{ padding: "8px" }} onClick={() => onClick(`${icons.pi}`)}>
+      <Button style={buttonStyle} onClick={() => onClick(`${icons.pi}`)}>
         {icons.pi}
       </Button>
-      <Button
-        style={{ padding: "8px" }}
-        onClick={() => onClick(`${icons.rho}`)}
-      >
+      <Button style={buttonStyle} onClick={() => onClick(`${icons.rho}`)}>
         {icons.rho}
       </Button>
       <Button
-        style={{ padding: "8px" }}
+        style={{ padding: "0px 2px 4px 2px", fontSize: "24px" }}
         onClick={() => onClick(`${icons.union}`)}
       >
         {icons.union}
       </Button>
       <Button
-        style={{ padding: "8px" }}
+        style={buttonStyle}
         onClick={() => onClick(`${icons.intersection}`)}
       >
         {icons.intersection}
       </Button>
-      <Button
-        style={{ padding: "8px" }}
-        onClick={() => onClick(`${icons.bowtie}`)}
-      >
+      <Button style={buttonStyle} onClick={() => onClick(`${icons.bowtie}`)}>
         {icons.bowtie}
       </Button>
-      <Button
-        style={{ padding: "8px" }}
-        onClick={() => onClick(`${icons.minus}`)}
-      >
+      <Button style={buttonStyle} onClick={() => onClick(`${icons.minus}`)}>
         {icons.minus}
       </Button>
     </ButtonGroup>
